7038df1f1b944c70c5123b7118a8ff7664754051
cbonato <c.bonato@hw.ac.uk>
2017-11-23 10:39:06 +0000

    -C

--- data_object.py
+++ data_object.py
@@ -1,3 +1,16 @@
+
+#!/usr/bin/env python2
+# 
+"""
+
+@author: Cristian Bonato
+
+Class that allows to save all variables in HDF5 files
+One can also save the code in specific modules, listed in self._called_modules
+
+Classes that need to load/save data from HDF% files can inherit this class
+
+"""
 import numpy as np
 
 class DataObjectHDF5 ():
@@ -20,8 +33,10 @@ class DataObjectHDF5 ():
 			elif isinstance(d[k], np.ndarray):
 				file_handle.create_dataset (k, data = d[k])
 
-	def load_file_to_dict (self, file_handle):
-		pass
+	def load_file (self, file_name):
+		f = h5py.File(file_name,'r')
+		for k in f.attrs.keys():
+			setattr (self, k, f.attrs [k])
 		
 	def store_function_code (self):
 		self.data_dict['code'] = {}
@@ -31,3 +46,29 @@ class DataObjectHDF5 ():
 			except:
 				print "Non-existing function: ", i
 		return self.data_dict['code']
+
+	def save_object_to_file (self, obj, f):
+
+		if type(f) is str:
+			if (f[-5:] != '.hdf5'):
+				f = f+'.hdf5'
+			file_handle = h5py.File(f, 'w')
+		else:
+			file_handle = f
+
+		for k in obj.__dict__.keys():
+			if (type(obj.__dict__[k]) in [int, float, str]):
+				file_handle.attrs[k] = getattr (obj, k)
+			elif isinstance(obj.__dict__[k], np.float64):
+				file_handle.attrs[k] = getattr (obj, k)
+			elif isinstance(obj.__dict__[k], np.int32):
+				file_handle.attrs[k] = getattr (obj, k)
+			elif isinstance(obj.__dict__[k], np.ndarray):
+				file_handle.create_dataset (k, data = getattr (obj, k))
+
+		if type(f) is str:
+			file_handle.close()
+
+
+
+
